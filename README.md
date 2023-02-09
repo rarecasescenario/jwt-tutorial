8def49bcde671fd5b166db9ebdea27c3d0b1c204
rarecasescenario <rare.case.scenario@gmail.com>
2023-02-09 11:27:23 -0500

    working stage #1

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 https://www.youtube.com/watch?v=KYNR5js2cXE&ab_channel=DanVega
 git@github.com:rarecasescenario/jwt-tutorial.git
 ```
+
+### Create a project using Spring Starter
+```
+Project: Gradle
+Language: Java
+Spring Boot: 3.0.2
+1. Spring Web
+2. OAuth2 Resource Server
+3. Spring Configuration Processor
+Java: 19
+```
--- src/main/java/com/jwt/JwtSecurityApplication.java
+++ src/main/java/com/jwt/JwtSecurityApplication.java
@@ -2,10 +2,16 @@ package com.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JwtSecurityApplication {
 
+	@GetMapping("/")
+    String home() {
+        return "Hello, JWT!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JwtSecurityApplication.class, args);
 	}
